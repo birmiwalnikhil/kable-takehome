e0f6060005a095a8e26459d7bba5b41a9953e47c
Nikhil Birmiwal <nikhilbirmiwal@gmail.com>
2022-05-29 17:46:27 +0000

    Implement a DEPEND command.

--- src/packages/Package.java
+++ src/packages/Package.java
@@ -40,5 +40,4 @@ public class Package {
   public void uninstall() {
     // TODO: Implement.
   }
-
 }
--- src/packages/PackageManager.java
+++ src/packages/PackageManager.java
@@ -1,5 +1,7 @@
 package kable.packages;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -8,26 +10,59 @@ import java.util.HashSet;
  */
 public class PackageManager {
 
-  /** 
-   * All packages that the PackageManager is aware of. A package
-   * may not be installed, e.g. it was introduced via a {@link #depend} call.
+  /**
+   * A mapping from PackageName -> Package for all packages that 
+   * the PackageManager is aware of. A package may not be installed, e.g.
+   * it was introduced via a {@link #depend} call.
    */
-  Set<Package> packages;
+  Map<String, Package> packages;
 
   public PackageManager() {
-    this.packages = new HashSet<>();
+    this.packages = new HashMap<>();
+  }
+
+  /**
+   * Introduce a list of dependencies required for {@code packageA}.
+   */
+  public void depend(String packageA, String dependency, String...
+otherDependencies) {
+    // PackageA must not already be defined, as per the spec.
+    if (packages.containsKey(packageA)) {
+      echo(
+        String.format("DEPEND on %s invalid; package already defined.",
+packageA));
+      return;
+    } 
+
+    Package primaryPackage = new Package(packageA);
+    Set<String> dependencies = new HashSet<>();
+    dependencies.add(dependency);
+    for (String otherDependency : otherDependencies) {
+      dependencies.add(otherDependency);
+    }
+
+    // For each dependency B, indicate that A is dependent on B,
+    // and that B is needed for A.
+    for (String b : dependencies) {
+      Package dep = packages.getOrDefault(b, new Package(b));
+      primaryPackage.addDependency(dep);
+      dep.addNeededFor(primaryPackage);
+    }
+
+    return;
   }
 
   // TODO: Implement.
-  public void depend(Package packageA, Package dependency1, Package...
-otherDepedencies) {}
+  public void install(String packageA) {}
 
   // TODO: Implement.
-  public void install(Package packageA) {}
-
-  // TODO: Implement.
-  public void remove(Package packageA) {}
+  public void remove(String packageA) {}
 
   // TODO: Implement.
   public void list()  {}
+
+  /** Echo some feedback to the user. */
+  private void echo(String log) {
+    System.out.println(log);
+  }
 }
