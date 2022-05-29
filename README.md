b2410a9185b51ee96efa7c17cde1342f7a500511
Nikhil Birmiwal <nikhilbirmiwal@gmail.com>
2022-05-29 17:27:54 +0000

    Update readme with expectations and introduce a skeleton Package class.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # kable-takehome
+#
+# A take home interview for Kable, implementing a basic PackageManager in Java.
+# There are four top level Commands we support:
+#   - `DEPEND <package> <dependency1> [other dependencies..]`, indicating that
+#   `dependency1` is needed to install `package`.
+#     - Note: As per the README, once set of dependencies has been declared for
+#     a package, they won't be modified.
+#   - `INSTALL <package>`, which will 1) install any depdenencies of A, followed by 2)
+#   installing A itself. Echoes back the feedback if A is already installed.
+#   - `REMOVE <package>` attempts to remove an installed package.
+#     - If the package is not installed, communicate that to the user..
+#     - If the package is installed but required by another package,
+#     communicate that to the user.
+#     - If, after the removal of the package, no dependency of `package` is
+#     required for any other installed package, remove the dependency.
+#   - `LIST`. List all the installed programs. 
---
+++ src/packages/Package.java
@@ -0,0 +1,44 @@
+package kable.packages;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/** Define a package that is provided by one of the top level commands. */
+public class Package {
+  
+  /** The name of the package, e.g. `vim`. */
+  private final String name;
+
+  /** All packages that this package depends on. */
+  private final Set<Package> dependencies;
+
+  /** All packages that this package is needed for. */
+  private final Set<Package> neededFor;
+
+  /** Whether this package is installed. */
+  private boolean isInstalled;
+
+  public Package(String name) {
+    this.name = name;
+    this.dependencies = new HashSet<>();
+    this.neededFor = new HashSet<>();
+    this.isInstalled = false;
+  }
+
+  public void addDependency(Package dependency) {
+    this.dependencies.add(dependency);
+  }
+
+  public void addNeededFor(Package upstream) {
+    this.neededFor.add(upstream);
+  }
+
+  public void install() {
+    // TODO: Implement.
+  }
+
+  public void uninstall() {
+    // TODO: Implement.
+  }
+
+}
